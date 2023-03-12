ceb1779f135a6e00d863dfc82350d1465c95ef97
Abhishek Singh <84565588+Quick-Silver-Code@users.noreply.github.com>
2023-03-13 00:07:18 +0530

    Enter a commit message here

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-
-from flask import Flask, render_template
-import os
+# app.py
+from flask import Flask, render_template, request
+from Script import plot_candlestick_with_atr_msd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    input_data = request.form['input_data']
+    output_data = plot_candlestick_with_atr_msd(input_data)
+    return render_template('result.html', output_data=output_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
